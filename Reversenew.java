49606fcf4195676eb8c46fb066259ab07ddff4b8
Thirumurugan <thiru15siva@gmail.com>
2017-07-07 10:59:38 +0530

    Rename Reverse.java to Reversenew.java

---
+++ Reversenew.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Reverse {
+public static void main(String args[]) throws Exception{ 
+      Scanner  n=new Scanner(System.in);
+      int s,k=1,i;
+    
+  String a=n.nextLine();
+   char aa[]=a.toCharArray();
+     char b[]=new char[100];
+  
+     
+         for(i=0;k<=a.length();i++) {
+                 
+           b[i]=aa[a.length()-k]; 
+               k++;
+               System.out.print(b[i]);
+           }
+          
+  }
+}
